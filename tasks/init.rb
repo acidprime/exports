4c91d6af93ff39d1f6d2287380015744a2e7b246
maju6406 <beersy@gmail.com>
2018-01-30 11:33:00 -0800

    Adding task

---
+++ tasks/init.rb
@@ -0,0 +1,37 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+#
+# Puppet Task to purge nodes
+# This can only be run against the Puppet Master.
+#
+# Parameters:
+#   * agent_certnames - A comma-separated list of agent certificate names.
+#
+require 'puppet'
+require 'open3'
+
+Puppet.initialize_settings
+
+unless Puppet[:server] == Puppet[:certname]
+  puts 'This task can only be run against the Master (of Masters)'
+  exit 1
+end
+
+def exports(resources)
+  command = "/opt/puppetlabs/puppet/bin/puppet node export"
+
+  unless resources.to_s.empty?
+    command << " --resources \"#{resources}\""
+  end
+
+  stdout, stderr, status = Open3.capture3(command)
+  {
+    stdout: stdout.strip,
+    stderr: stderr.strip,
+    exit_code: status.exitstatus,
+  }
+end
+
+params = JSON.parse(STDIN.read)
+resources = params['resources']
+
+exports(resources)
